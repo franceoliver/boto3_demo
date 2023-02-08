41e35da0df4cdf633e0dfa84ed1a4ffdea7f7241
Oliver France <opcfrance@gmail.com>
2023-02-08 11:47:09 +1100

    adding final changes

--- S3/src/s3_operations.py
+++ S3/src/s3_operations.py
@@ -43,8 +43,57 @@ def list_buckets():
 def get_bucket_policy():
     return s3_client().get_bucket_policy(Bucket=BUCKET_NAME)
 
+def get_bucket_encryption():
+    return s3_client().get_bucket_encryption(Bucket=BUCKET_NAME)
+
+def update_bucket_policy(bucket_name):
+    bucket_policy = {
+        'Version': '2012-10-17',
+        'Statement': [
+            {
+                'Sid': 'AddPerm',
+                'Effect': 'Allow',
+                'Principal': '*',
+                'Action': [
+                    's3:DeleteObject',
+                    's3:GetObject',
+                    's3:PutObject'
+                ],
+                'Resource': 'arn:aws:s3:::' + bucket_name + '/*'
+            }
+        ]
+    }
+
+    policy_string = json.dumps(bucket_policy)
+
+    return s3_client().put_bucket_policy(
+        Bucket=bucket_name,
+        Policy=policy_string
+    )
+
+def server_side_encrypt_bucket():
+    return s3_client().put_bucket_encryption(
+        Bucket=BUCKET_NAME,
+        ServerSideEncryptionConfiguration={
+            "Rules": [
+                {
+                    "ApplyServerSideEncryptionByDefault" : {
+                        "SSEAlgorithm": "AES256"
+                    }
+                }
+            ]
+        }
+    )
+
+def delete_bucket():
+    return s3_client().delete_bucket(Bucket=BUCKET_NAME)
+
 if __name__ == '__main__':
     # print(create_bucket(BUCKET_NAME))
     # print(create_bucket_policy())
     # print(list_buckets())
-    print(get_bucket_policy())
+    # print(get_bucket_policy())
+    # print(get_bucket_encryption())
+    # print(update_bucket_policy(BUCKET_NAME))
+    # print(server_side_encrypt_bucket())
+    print(delete_bucket())
