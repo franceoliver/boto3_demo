48b773ea3ef47f36abe24cb1dcf436bd72da30c3
Oliver France <opcfrance@gmail.com>
2023-02-09 17:02:43 +1100

    updating for ec2 changes

--- src_ec2/test_deployment.py
+++ src_ec2/test_deployment.py
@@ -59,9 +59,95 @@ def main():
     # Add name tag to private subnet
     vpc.add_name_tag(private_subnet_id, 'Boto3-Private-Subnet')
 
-    # _12 Instances
+    # EC2 Instances
     ec2 = EC2(ec2_client)
+
+    # # Create a keypair
+    key_pair_name = "boto3-KeyPair"
+    key_pair_response = ec2.create_key_pair(key_pair_name)
+    #
+    print(f"Created Key Pair with name {key_pair_name} : {str(key_pair_response)}")
+
+    # Create a Security Group
+    public_security_group_name = 'Boto3-Public-SG'
+    public_security_group_description = 'Public Security Group for Public Subnet Internet Access'
+    public_security_group_response = ec2.create_security_group(public_security_group_name, public_security_group_description, vpc_id)
+
+
+    public_security_group_id = public_security_group_response['GroupId']
+
+    # Add Public Access to Security Group
+    ec2.add_inbound_rule_to_sg(public_security_group_id)
+
+    print('Added public access rule to Security Group ' + public_security_group_name)
+
+    user_data = """#!/bin/bash
+                yum update -y
+                yum install httpd24 -y
+                service httpd start
+                chkconfig httpd on
+                echo "<html><body><h1>Hello from <b>Boto3</b> using Python!</h1></body></html>" > /var/www/html/index.html"""
+
+    ami_id = 'ami-1b316af0'
+
+    # Launch a public EC2 Instance
+    ec2.launch_ec2_instance(ami_id, key_pair_name, 1, 1, public_security_group_id, public_subnet_id, user_data)
+    print(f"Launching Public EC2 Instance using AMI: {ami_id}")
+
+    # Adding another Security Group for Private EC2 Instance
+    private_security_group_name = 'Boto3-Private-SG'
+    private_security_group_description = 'Private Security Group for Private Subnet'
+    private_security_group_response = ec2.create_security_group(private_security_group_name, private_security_group_description, vpc_id)
+
+    private_security_group_id = private_security_group_response['GroupId']
+
+    # Add rule to private security group
+    ec2.add_inbound_rule_to_sg(private_security_group_id)
+
+    # Launch a private EC2 Instance
+    ec2.launch_ec2_instance(ami_id, key_pair_name, 1, 1, private_security_group_id, private_subnet_id, """""")
+
+def describe_instances():
+    ec2_client = EC2Client().get_client()
+    ec2 = EC2(ec2_client)
+
+    ec2_response = ec2.describe_ec2_instances()
+
+    print(str(ec2_response))
+
+
+def modify_instance():
+    ec2_client = EC2Client().get_client()
+    ec2 = EC2(ec2_client)
+
+    ec2.modify_ec2_instance('i-0ef56acb13a7eff31')
+
+
+def stop_instance():
+    ec2_client = EC2Client().get_client()
+    ec2 = EC2(ec2_client)
+
+    ec2.stop_instance('i-0ef56acb13a7eff31')
+
+
+def start_instance():
+    ec2_client = EC2Client().get_client()
+    ec2 = EC2(ec2_client)
+
+    ec2.start_instance('i-0ef56acb13a7eff31')
+
+
+def terminate_instance():
+    ec2_client = EC2Client().get_client()
+    ec2 = EC2(ec2_client)
+
+    ec2.terminate_instance('i-0ef56acb13a7eff31')
+
 
 if __name__ == '__main__':
     # main()
-    pass
+    # describe_instances()
+    modify_instance()
+    # stop_instance()
+    # start_instance()
+    terminate_instance()
